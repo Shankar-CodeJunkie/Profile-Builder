6e1ba425be4b20160ef485d251f89f5a7978d534
Shankar-CodeJunkie <emailmeforshankar@gmail.com>
2022-12-06 23:43:14 +0530

    fix: fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-/*const path = require('path')
+const path = require('path')
 const nextConfig = {
-  sass: true, // use .scss files
+  //sass: true, // use .scss files
   //modules: true, // style.(m|module).css & style.(m|module).scss for module files
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'scss'), path.join(__dirname, 'node_modules')],
-  },
+  //sassOptions: {
+    //includePaths: [path.join(__dirname, 'scss'), path.join(__dirname, 'node_modules')],
+  //},
   poweredByHeader: false
 }
 
-module.exports = nextConfig*/
+module.exports = nextConfig
